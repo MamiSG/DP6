1846b853d3f5df6879f0916f1093b286deaa8871
Mami <sgmami37@gmail.com>
2024-08-22 22:30:05 -0300

    tentativa de crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { ScryfallService } from './scryfall/scryfall.service';
 import { ScryfallController } from './scryfall/scryfall.controller';
 import { HttpModule } from '@nestjs/axios';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, MongooseModule.forRoot('mongodb://localhost:27017'), UserModule,],
   controllers: [AppController, ScryfallController],
   providers: [AppService, ScryfallService],
 })
